7e4864ffa1e7d99e5ff3a13b6f6c36d428f0b3a0
orangepizero2 <gngppz@gmail.com>
2022-11-15 07:48:43 +0000

    update

--- orangepizero2/rc.sh
+++ orangepizero2/rc.sh
@@ -2,7 +2,7 @@
 for a in {1..30}
 do
 	if [[ -n $(docker ps -q -f name="openwrt_master") ]];then
-		echo "nameserver=223.5.5.5" > /etc/resolv.conf
+		echo "nameserver 223.5.5.5" > /etc/resolv.conf
 		route add default gw 192.168.4.1 br-$(docker network ls | grep orangepizero2_lan | awk -F ' ' '{print $1}')
 		break;
 	fi
