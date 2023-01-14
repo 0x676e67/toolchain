47c2b01c9497c966db4cc753260fc9549d705218
gngpp <gngppz@gmail.com>
2023-01-14 03:06:37 +0000

    update

--- orangepizero2/init.sh
+++ orangepizero2/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
- sed -i '$i ip link set eth0 promisc on'  /etc/rc.local
- sed -i '$i nohup /root/docker-profiles/orangepizero2/rc.sh &'  /etc/rc.local
+sed -i '$i ip link set eth0 promisc on'  /etc/rc.local
+sed -i '$i nohup $(pwd)/rc.sh &'  /etc/rc.local
+echo "* * * * * nohup $(pwd)/ping.sh &" >> /etc/crontab
