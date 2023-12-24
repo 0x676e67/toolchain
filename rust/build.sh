a833576aa2f58334cdf95b62d636fa272ba39066
gngpp <gngppz@gmail.com>
2023-12-24 07:53:42 +0000

    Update

--- rust/build.sh
+++ rust/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64,linux/arm64 --tag gngpp/cargo-zigbuild:latest --tag ghcr.io/gngpp/cargo-zigbuild:latest . --push
+docker buildx build --platform linux/amd64 --tag ghcr.io/gngpp/cargo-zigbuild:latest . --push
