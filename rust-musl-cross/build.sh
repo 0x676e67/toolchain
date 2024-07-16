cc133c721b090cf0aa33a0ee6aacd45c3df1010a
0x676e67 <gngppz@gmail.com>
2024-07-16 19:57:32 +0800

    Update

---
+++ rust-musl-cross/build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -ex
+
+# x86_64-pc-windows-msvc
+cd cargo-xwin
+docker build -t ghcr.io/0x676e67/rust-musl-cross:x86_64-pc-windows-msvc . --push
+cd -
+
+# x86_64-unknown-linux-musl
+docker build -t ghcr.io/0x676e67/rust-musl-cross:x86_64-unknown-linux-musl . --push
+
+# aarch64-unknown-linux-musl
+docker build --build-arg TARGET=aarch64-unknown-linux-musl \
+    --build-arg RUST_MUSL_MAKE_CONFIG=config.mak \
+    -t ghcr.io/0x676e67/rust-musl-cross:aarch64-unknown-linux-musl . --push
+
+# armv7-unknown-linux-musleabihf
+docker build --build-arg TARGET=armv7-unknown-linux-musleabihf \
+    --build-arg RUST_MUSL_MAKE_CONFIG=config.mak \
+    -t ghcr.io/0x676e67/rust-musl-cross:armv7-unknown-linux-musleabihf . --push
+
+# armv7-unknown-linux-musleabi
+docker build --build-arg TARGET=armv7-unknown-linux-musleabi \
+    --build-arg RUST_MUSL_MAKE_CONFIG=config.mak \
+    -t ghcr.io/0x676e67/rust-musl-cross:armv7-unknown-linux-musleabi . --push
+
+# arm-unknown-linux-musleabi
+docker build --build-arg TARGET=arm-unknown-linux-musleabi \
+    --build-arg RUST_MUSL_MAKE_CONFIG=config.mak \
+    -t ghcr.io/0x676e67/rust-musl-cross:arm-unknown-linux-musleabi . --push
+
+# arm-unknown-linux-musleabihf
+docker build --build-arg TARGET=arm-unknown-linux-musleabihf \
+    --build-arg RUST_MUSL_MAKE_CONFIG=config.mak \
+    -t ghcr.io/0x676e67/rust-musl-cross:arm-unknown-linux-musleabihf . --push
+
+# armv5te-unknown-linux-musleabi
+docker build --build-arg TARGET=armv5te-unknown-linux-musleabi \
+    --build-arg RUST_MUSL_MAKE_CONFIG=config.mak \
+    -t ghcr.io/0x676e67/rust-musl-cross:armv5te-unknown-linux-musleabi . --push
+
+# i686-unknown-linux-musl i686-unknown-linux-gnu
+docker build --build-arg TARGET=i686-unknown-linux-musl \
+    --build-arg RUST_MUSL_MAKE_CONFIG=config.mak \
+    -t ghcr.io/0x676e67/rust-musl-cross:i686-unknown-linux-musl \
+    -t ghcr.io/0x676e67/rust-musl-cross:i686-unknown-linux-gnu . --push
+
+# i586-unknown-linux-musl i586-unknown-linux-gnu
+docker build --build-arg TARGET=i586-unknown-linux-musl \
+    --build-arg RUST_MUSL_MAKE_CONFIG=config.mak \
+    -t ghcr.io/0x676e67/rust-musl-cross:i586-unknown-linux-musl \
+    -t ghcr.io/0x676e67/rust-musl-cross:i586-unknown-linux-gnu . --push
